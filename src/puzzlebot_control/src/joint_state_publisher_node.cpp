262415979f83b0edccdef2a6a974c1e0c17fc6f4
Morr6450 <a00828311@tec.mx>
2024-05-02 16:33:23 -0600

    add joint publisher node

---
+++ src/puzzlebot_control/src/joint_state_publisher_node.cpp
@@ -0,0 +1,63 @@
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/pose.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "std_msgs/msg/float64.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+
+class JointStatePubNode : public rclcpp::Node {
+public:
+    JointStatePubNode() : Node("joint_state_publisher") {
+        double dt = 0.01;
+        jointPub = this->create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
+
+        leftSub = this->create_subscription<std_msgs::msg::Float64>(
+            "wl", 1, [&](const std_msgs::msg::Float64::SharedPtr msg) {
+                this->wl = msg->data;
+            });
+
+        rightSub = this->create_subscription<std_msgs::msg::Float64>(
+            "wr", 1, [&](const std_msgs::msg::Float64::SharedPtr msg) {
+                this->wr = msg->data;
+            });
+
+        timer = this->create_wall_timer(std::chrono::duration<double>(dt), std::bind(&JointStatePubNode::update, this));
+    }
+
+protected:
+    void update() {
+        // Hardcode for now
+        double dt = 0.01;
+
+        left_wheel += wl * dt;
+        right_wheel += wr * dt;
+
+        sensor_msgs::msg::JointState joint_msg;
+        joint_msg.name = {"leftWheel", "rightWheel"};
+        joint_msg.position = {left_wheel, right_wheel};
+
+        joint_msg.header.stamp = this->now();
+        jointPub->publish(joint_msg);
+    }
+
+private:
+    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr jointPub;
+    rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr leftSub, rightSub;
+    rclcpp::TimerBase::SharedPtr timer;
+
+    double wl{0}, wr{0};
+    double left_wheel{0}, right_wheel{0};
+
+    // Simulation constants
+    static constexpr double dt{0.01};
+    static constexpr double r{0.05}; // wheel radius
+    static constexpr double l{0.19}; // wheel base
+};
+
+int main(int argc, char **argv) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<JointStatePubNode>());
+    rclcpp::shutdown();
+    return 0;
+}
