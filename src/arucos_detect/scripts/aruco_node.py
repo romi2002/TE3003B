fd9bd4cf2783f90f3aaafebe0a964febf6f23f26
robotonoto <robotonoto@puzzlebot.com>
2024-05-27 19:19:03 +0100

    Added aruco pkg

---
+++ src/arucos_detect/scripts/aruco_node.py
@@ -0,0 +1,122 @@
+#Ppmike aniade a un array los arucos que se estan detectando a diferencia del Airlab donde se incluye en un solo mensaje todo
+#Airlab hace uso de sus propias funciones de aruco pose estimation, ppmike usa opencv (se utilizar opencv)
+
+#Ros2 Imports
+import rclpy
+import rclpy.node import Node
+from rclpy.qos import qos_profile_sensor_data
+from cv_bridge import CvBridge
+import message_filters
+
+# Python imports
+import numpy as np
+import cv2
+from cv_bridge import CvBridge
+import argparse
+
+# ROS2 message imports
+from sensor_msgs.msg import CameraInfo
+from sensor_msgs.msg import Image, CameraInfo
+from geometry_msgs.msg import PoseArray
+from aruco_interfaces.msg import ArucoMarkers, ArucosDetected
+from rcl_interfaces.msg import ParameterDescriptor, ParameterType
+
+
+# Define command line arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-vs", "--video_source", type=str, default="/video_source/raw",
+                help="Video source ROS topic")
+ap.add_argument("-ci", "--camera_info", type=str, default="/camera_info",
+                help="Camera info ROS topic")
+ap.add_argument("-t", "--type", type=str, default="DICT_ARUCO_ORIGINAL",
+                help="Type of ArUCo tag to detect")
+
+# Use parse_known_args to ignore unknown args
+args, unknown = ap.parse_known_args()
+args = vars(args)
+
+# Verify and get the ArUco dictionary
+ARUCO_DICT = {
+    "DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+    "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+    "DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+    "DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+    "DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+    "DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+    "DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+    "DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+    "DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+    "DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+    "DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+    "DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+    "DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+    "DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+    "DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+    "DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+    "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+    "DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+    "DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+    "DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+    "DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+}
+
+if args["type"] not in ARUCO_DICT:
+    rospy.logerr("ArUCo tag type '{}' is not supported".format(args["type"]))
+    sys.exit(0)
+
+aruco_dict = cv2.aruco.Dictionary_get(ARUCO_DICT[args["type"]])
+aruco_params = cv2.aruco.DetectorParameters_create()
+
+
+class ArucoNode(Node):
+    
+    def __init__(self):
+        super().__init__('aruco_node')
+        timer_period = 0.05 #seconds
+        self.timer =  self.create_timer(timer_period, self.process_aruco)
+        self.bridge = CvBridge()
+        self.camera_matrix = None
+        self.distortion_coefficients = None
+        self.marker_size = 0.1 #Marker Size in Meters
+
+        #Setup Publisher
+        self.aruco_publisher_ = self.create_publisher(ArucosDetected, 'arucos_detected', 10)
+
+        #Setup Subscriber
+        self.image = self.create_subscription(Image, args["video_source"], self.imageproc_cb,10)
+        self.caminfo_sub = self.create_subscription(CameraInfo, args["camera_info", self.caminfo_cb,10])
+
+    #Camera Info Callback
+    def caminfo_cb(self, data):
+        self.camera_matrix = np.array(data.k).reshape(3, 3)
+        self.distortion_coefficients = np.array(data.d)
+
+    #Image Processing Callback
+    def imageproc_cb(self, data):
+        opencv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        grayscale = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2GRAY)
+        self.corners, self.ids, self.rejected = cv2.aruco.detectMarkers(grayscale, aruco_dict, parameters=aruco_params)
+        if self.ids is not None:
+            self.process_aruco()
+    
+    #Process Detected Aruco Markers
+    def process_aruco(self):
+        detected_arucos = []
+        rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(self.corners, self.marker_size, self.camera_matrix, self.distortion_coefficients)
+        for i, corner in enumerate(self.corners):
+            center = corner.reshape((4,2)).mean(axis=0)
+            pose = [tvecs[i][0][0],tvecs[i][0][1],tvecs[i][0][2]] #position in 3D space
+            detected_arucos.append(ArucosDetected(id=int(self.ids[i][0]), pose=pose))
+        self.aruco_publisher_.publish(ArucosDetected(arucos_detected=detected_arucos))
+        self.get_logger().info('Publishing ArucosDetected')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    aruco_node = ArucoNode()
+    rclpy.spin(aruco_node)
+    aruco_node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ src/arucos_detect/scripts/camera_info.py
@@ -0,0 +1,57 @@
+#Ros2 Imports
+import rclpy
+import rclpy.node import Node
+
+# Python imports
+import numpy as np
+import cv2
+import yaml
+import os
+
+# ROS2 message imports
+from sensor_msgs.msg import CameraInfo
+
+
+#Calibration YAML extract CameraInfo
+yaml_path = os.path.expanduser("TE3003B/src/arucos_detect/yaml/ost.yaml")
+
+def extract_camera_info(yaml_file):
+    """Load camera info from a YAML file."""
+    with open(yaml_file, "r") as file_handle:
+        calib_data = yaml.safe_load(file_handle) #Modified
+
+    camera_info_msg = CameraInfo()
+    camera_info_msg.width = calib_data["image_width"]
+    camera_info_msg.height = calib_data["image_height"]
+    camera_info_msg.distortion_model = calib_data["distortion_model"]
+    camera_info_msg.D = calib_data["distortion_coefficients"]["data"]
+    camera_info_msg.K = calib_data["camera_matrix"]["data"]
+    camera_info_msg.R = calib_data["rectification_matrix"]["data"]
+    camera_info_msg.P = calib_data["projection_matrix"]["data"]
+
+    return camera_info_msg
+
+
+class CalibrationPublisher(Node):
+
+    def __init__(self):
+        super().__init__('calibration_publisher')
+        self.publisher_ = self.create_publisher(CameraInfo, "camera_info", 10)
+        timer_period = 0.5
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def timer_callback(self):
+        msg = CameraInfo()
+        msg.data = extract_camera_info(yaml_path)
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing CameraInfo')
+
+def main(args=None):
+    rclpy.init(args=args)
+    calibration_publisher = CalibrationPublisher()
+    rclpy.spin()
+    calibration_publisher.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
