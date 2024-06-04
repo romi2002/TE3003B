a3e999ce31bc80fd28f7893515dda4c58127244c
robotonoto <robotonoto@puzzlebot.com>
2024-06-04 01:06:29 +0100

    Gripper Testing v2 - missing actual states

--- src/puzzlebot_control/scripts/gripper_test.py
+++ src/puzzlebot_control/scripts/gripper_test.py
@@ -21,7 +21,7 @@ class GripperNode(Node):
         # SUBSCRIBERS      
         self.create_subscription(ArucosDetected, '/arucos_detected', self.aruco_callback, 10)
         self.create_subscription(Bool, '/gripper_start',self.cube_callback,10)
-        self.create_timer(1.0, self.timer_callback)
+        self.create_timer(0.01, self.timer_callback)
 
         # PUBLSHERS
         self.servo_pub = self.create_publisher(Float32, '/ServoAngle', 10)
@@ -37,18 +37,27 @@ class GripperNode(Node):
         self.aruco = ArucoMarkers()
 
         # Coordanates of the center of the image
-        self.CENTER_IMG_X = 640 # 1280/2 (WIDTH_IMG/2)
-        self.CENTER_IMG_Y = 360 # 720/2 (HEIGHT_IMG/2)
+        self.CENTER_IMG_X = (1280 / 2) + 400  # 1280/2 (WIDTH_IMG/2)
+        self.CENTER_IMG_Y = 720 / 2 # 720/2 (HEIGHT_IMG/2)
+
+        self.error_x_integral = 0
+        self.error_ang_integral = 0
 
         # State variables
         self.actual_state = 'FINISHED'
-        self.cube_id = 9
-        self.kp_linear, self.kp_angular = 0.1, 0.1
-        self.error_tol = 10.0
-        self.servo_grip_angle = 20.0
+        self.cube_id = 6
+        self.kp_linear, self.kp_angular = 0.55, 0.45
+        self.ki_linear, self.ki_angular = 0.0001, 0.0001
+        self.kd_linear, self.kd_angular = 0.0055, 0.0055
+        self.error_tol_x = 0.1
+        self.error_tol_y = 0.1
+        self.servo_grip_angle = 25.0
+        self.last_error_x = 0.0
+        self.last_error_y = 0.0
 
     def aruco_callback(self, msg):
         self.aruco_msg = msg
+        self.aruco_msg.detections.sort(key = lambda tag: tag.area.data)
 
     def cube_callback(self, msg):
         self.gripper_start = msg.data
@@ -59,8 +68,12 @@ class GripperNode(Node):
             self.actual_state = 'SEARCHING'
         
         if self.actual_state == 'SEARCHING':
+            self.gripper_state = False
+            open_angle = Float32()
+            open_angle.data = 70.0
+            self.servo_pub.publish(open_angle)
             self.get_logger().info('Searching')
-            self.robot_velocity.angular.z = 0.2
+            self.robot_velocity.angular.z = 0.3
             self.robot_velocity.linear.x = 0.0
             for tag in self.aruco_msg.detections:
                 if tag.marker_id != self.cube_id:
@@ -83,26 +96,34 @@ class GripperNode(Node):
             delta_x = self.CENTER_IMG_X - tag.centroid.x
             delta_y = self.CENTER_IMG_Y - tag.centroid.y
             error_x = delta_x / self.CENTER_IMG_X
-            error_y = delta_y / self.CENTER_IMG_Y
-            distance_error = np.hypot(delta_x, delta_y)
+            error_y = abs(delta_y / self.CENTER_IMG_Y)
+            distance_error = np.hypot(error_x, error_y)
             gripper_state.distance_error = distance_error
-            
-            if distance_error < self.error_tol:
+            gripper_state.error_x = error_x
+            gripper_state.error_y = error_y
+            if abs(error_x) < self.error_tol_x and abs(error_y) < self.error_tol_y:
                 self.robot_velocity.angular.z = 0.0
                 self.robot_velocity.linear.x = 0.0
                 self.velocity_pub.publish(self.robot_velocity)
                 self.get_logger().info('Aligned')
                 self.actual_state = 'GRIP'
             else:
-                self.robot_velocity.angular.z = self.kp_angular * error_x
-                self.robot_velocity.linear.x = self.kp_linear * error_y
+                self.robot_velocity.angular.z = self.kp_angular * error_x + self.ki_angular * self.error_x_integral + self.kd_angular * (error_x - self.last_error_x) 
+                self.robot_velocity.linear.x = self.kp_linear * error_y + self.ki_linear * self.error_ang_integral + self.kd_linear * (error_y - self.last_error_y)
+                self.error_x_integral += error_x
+                self.error_ang_integral += error_y
+                self.last_error_x = error_x
+                self.last_error_y = error_y
                 self.velocity_pub.publish(self.robot_velocity)
 
         if self.actual_state == 'GRIP':
+            zero_vel = Twist()
+            self.velocity_pub.publish(zero_vel)
             self.get_logger().info('Gripping')
             self.servo_angle.data = self.servo_grip_angle
-            self.actual_state = 'FINISHED'
+            #self.actual_state = 'FINISHED'
             self.servo_pub.publish(self.servo_angle)
+            rclpy.shutdown()
 
         gripper_state.state = self.actual_state
         self.gripper_pub.publish(gripper_state)
